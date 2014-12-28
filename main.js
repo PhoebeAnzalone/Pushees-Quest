92145b54dc1a8fa4af35d0a1dc9fa915375076ba
Shelley Anzalone <shelley@stevenanzalone.com>
2014-12-27 19:17:01 -0800

    Add collision detection

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ function preload() {
 	game.load.image('wall',   'sprites/wall.png');
 	game.load.image('exit',   'sprites/exit.png');
 	game.load.image('used',   'sprites/used.png');
-
 }
 
 var player;
@@ -21,6 +20,10 @@ var walls;
 function create() {
 	walls = game.add.group();
 	player = game.add.sprite(0, 0, 'pushee');
+
+	var newX = player.x;
+	var newY = player.y;
+
 	walls.create(50*4, 50*3, 'wall');
 
 	game.scale.setGameSize(player.width*16, player.height*8);
@@ -31,9 +34,6 @@ function create() {
 	game.physics.enable(walls, Phaser.Physics.ARCADE);
 	game.physics.enable(player, Phaser.Physics.ARCADE);
 
-
-	//  In this example we'll create 4 specific keys (up, down, left, right) and monitor them in our update function
-
 	upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
 	downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
 	leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
@@ -41,36 +41,33 @@ function create() {
 }
 
 function update() {
+	newX = player.x;
+	newY = player.y;
 	if (upKey.isDown && !keyIsDown)
 	{
-		player.y -= player.height;
+		newY = player.y - player.height;
 		keyIsDown = true;
 	}
 	else if (downKey.isDown && !keyIsDown)
 	{
-		player.y += player.height;
+		newY = player.y + player.height;
 		keyIsDown = true;
 	}
 	else if (leftKey.isDown && !keyIsDown)
 	{
-		player.x -= player.width;
+		newX = player.x - player.width;
 		keyIsDown = true;
 	}
 	else if (rightKey.isDown && !keyIsDown)
 	{
-		player.x += player.width;
+		newX = player.x + player.width;
 		keyIsDown = true;
 	}
 	if (!upKey.isDown && !downKey.isDown && !leftKey.isDown && !rightKey.isDown) {
 		keyIsDown = false;
 	}
-
-	game.physics.arcade.getObjectsAtLocation(200, 150, walls, function(foo, bar){
-		// console.log(foo, bar);
-	});
-
-/*	if(game.physics.arcade.overlap(player, walls)) {
-		player.x = 200;
-		player.y = 200;
-	}*/
+	if (!game.physics.arcade.getObjectsAtLocation(newX, newY, walls).length) {
+		player.x = newX;
+		player.y = newY;
+	}
 }
